fc7d341f908b5e576770dfa6fcb95350c78d7804
Manan <manan96.mb@gmail.com>
2019-03-13 21:49:39 -0400

    [UPDATE] max_flow

--- Binary-Image-Segmentation/main.cpp
+++ Binary-Image-Segmentation/main.cpp
@@ -1,246 +1,193 @@
-#include "pch.h"
 #include <opencv2/opencv.hpp>
-#include <fstream>
+#include<fstream>
 #include <utility>
-#include <queue>
+#include<queue>
+#include<vector>
+
 using namespace cv;
 using namespace std;
 
-void convertToGrayscale(Mat &in_image, Mat &gray_image, Mat &grad_x, Mat &grad_y) {
-	GaussianBlur(in_image, in_image, Size(3, 3), 0, 0, BORDER_DEFAULT);
-	cvtColor(in_image, gray_image, COLOR_BGR2GRAY);
-	Scharr(gray_image, grad_x, CV_16S, 1, 0, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_x, grad_x);
-	Scharr(gray_image, grad_y, CV_16S, 0, 1, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_y, grad_y);
-}
-void printList(vector< list< pair<int, int> > > &adjlist, vector<pair<int, int>> at) {
-	/*for (int i = 0; i < adjlist.size(); ++i) {
-		printf("adjacencyList[%d] ", i);
+bool *ispixelChecked;
+int maxIntPixel = 0;
+vector < pair < int, int > > differMatrix2;
 
-		list< pair<int, int> >::iterator itr = adjlist[i].begin();
-
-		while (itr != adjlist[i].end()) {
-			printf(" -> %d(%d)", (*itr).first, (*itr).second);
-			++itr;
-		}
-		printf("\n");
-	}*/
-	
-	list< pair<int, int> >::iterator itr = adjlist[10].begin();
-	while (itr != adjlist[10].end()) {
-		printf(" -> %d(%d)", (*itr).first, (*itr).second);
-		
-		cout<<at[(*itr).first].first<<endl;
-		cout<<"sdjsbfjk " << at[10].first;
-		at[10].first = (*itr).first;
-		cout << "sadjkfsd " << at[10].first;
-		++itr;
-	}
-	
-	
-}
-
-void setEdge(vector< list< pair<int, int> > > &adjlist, int k, int v, int weight)
-{
-	adjlist[k].push_back(make_pair(v,weight));
-}
-
-void getEdgeWeight() {
-
-}
-
-int inspect1(vector<int> newvector, int a) {
-	for (int i = 0; i < newvector.size(); i++) {
-		if (newvector[i] == a) {
+int inspectelement(vector < int > new_vector, int a) {
+	int size = new_vector.size();
+	for (int i = 0; i < size; i++)
+		if (new_vector[i] == a)
 			return 0;
-		}
-	}
 	return 1;
 }
-int inspect(vector<int> t, int l) {
-	for (int i = 0; i < t.size(); i++) {
-		if (t[i] == l) {
+int inspect(vector<int> sinkNodes, int l) {
+	int size = sinkNodes.size();
+	for (int i = 0; i < size; i++) {
+		if (sinkNodes[i] == l) {
 			return 1;
-		}	
+		}
 	}
 	return 0;
 }
-void BFS(queue<int> qcolor, vector<pair<int,int>> comb1, vector< list< pair<int, int> > > &adjlist, Mat &out_image, int width) {
-	while (!qcolor.empty()) {
 
-		int last = qcolor.front();
-		qcolor.pop();
+void addIntoQueue(queue < vector < int > > &Queue, vector<int> &sourceNodes) {
+	for (int i = 0; i < sourceNodes.size(); i++) {
+		vector<int> temp_vector;
+		temp_vector.push_back(sourceNodes[i]);
+		Queue.push(temp_vector);
+	}
+}
 
-			list< pair<int, int> >::iterator itr = adjlist[last].begin();
-			while (itr != adjlist[last].end()) {
-				if ((comb1[(*itr).first].second == 0) && (*itr).second > 0) {
+void coloring(int imageHeight, int imageWidth, Mat &out_image, vector<int> &sinkNodes, vector<int> &sourceNodes, queue < int > &colored_pixels) {
+	for (int w = 0; w < imageHeight; w++) {
+		for (int h = 0; h < imageWidth; h++) {
+			Vec3b color;
+			color[0] = 0;
+			color[1] = 0;
+			color[2] = 0;
+			out_image.at<Vec3b>(w, h) = color;
+		}
+	}
+	int firstNode = sourceNodes[0];
+	Vec3b color2;
+	color2[0] = 255;
+	color2[1] = 255;
+	color2[2] = 255;
 
-					Vec3b pixel2;
-					pixel2[0] = 0;
-					pixel2[1] = 0;
-					pixel2[2] = 225;
-					int x3, x4;
-					x3 = ((*itr).first)/ width;
-					x4 = ((*itr).first) % width;
-					out_image.at < Vec3b >(x3, x4) = pixel2;
+	colored_pixels.push(firstNode);
+	int x, y;
+	x = firstNode / imageWidth;
+	y = firstNode % imageWidth;
+	out_image.at < Vec3b >(x, y) = color2;
+}
 
-					qcolor.push((*itr).first);
-					comb1[(*itr).first].second = 1;
-
-				}
-				++itr;
+void BFS(Mat &out_image, vector < pair < int, int > >  *weightVector, int imageWidth,
+	vector < pair < int, int > > differMatrix2, queue < int > &colored_pixels) {
+	while (!colored_pixels.empty()) {
+		int firstPixel = colored_pixels.front();
+		colored_pixels.pop();
+		for (int i = 0; i < weightVector[firstPixel].size(); ++i) {
+			if (differMatrix2[weightVector[firstPixel][i].first].second == 0 && weightVector[firstPixel][i].second > 0) {
+				Vec3b color2;
+				color2[0] = 255;
+				color2[1] = 255;
+				color2[2] = 225;
+				int x3, x4;
+				x3 = (weightVector[firstPixel][i].first) / imageWidth;
+				x4 = (weightVector[firstPixel][i].first) % imageWidth;
+				out_image.at < Vec3b >(x3, x4) = color2;
+				colored_pixels.push(weightVector[firstPixel][i].first);
+				differMatrix2[weightVector[firstPixel][i].first].second = 1;
+			}
+		}
+	}
+}
+bool bfs2(vector<int> &sinkNodes,vector<int> &sourceNodes, vector < pair < int, int > > &differMatrix,
+	int total_pixel, vector < pair < int, int > >  *weightVector, vector < int > route) {
+	queue < vector < int > > Queue;
+	memset(ispixelChecked, 0, sizeof(ispixelChecked));
+	while (!Queue.empty()) {
+		Queue.pop();
+	}
+	addIntoQueue(Queue, sourceNodes);
+	vector < int > pathVector;
+	while (!Queue.empty()) {
+		pathVector = Queue.front();
+		Queue.pop();
+		int lastPixel = pathVector.back();
+		
+		for (int i = 0; i < weightVector[lastPixel].size(); ++i) {
+			if (!ispixelChecked[weightVector[lastPixel][i].first] && inspectelement(pathVector, weightVector[lastPixel][i].first) && weightVector[lastPixel][i].second > 0 && differMatrix[weightVector[lastPixel][i].first].second == 0) {
+				vector < int > temp_vector(pathVector);
+				temp_vector.push_back(weightVector[lastPixel][i].first);
+				differMatrix[weightVector[lastPixel][i].first].second = 1;
+				ispixelChecked[weightVector[lastPixel][i].first] = true;
+				Queue.push(temp_vector);
+			}
+		}
+		if (inspect(sinkNodes, lastPixel) == 1) {
+			route.clear();
+			route = pathVector;
+			pathVector.clear();
+			differMatrix.clear();
+			for (int i = 0; i < total_pixel; i++) {
+				differMatrix.push_back(make_pair(i, 0));
+				ispixelChecked[i] = false;
+			}
+			return true;
 		}
 	}
 }
 
-void coloring() {
+//implementation of max_flow min_cut algorithm
+void maxFlow_minCut(vector<int> &sinkNodes, vector<int> &sourceNodes, vector < pair < int, int > > &differMatrix,
+	int total_pixel, vector < pair < int, int > >  *weightVector, int imageHeight, int imageWidth, Mat &out_image) {
 
+	
+	queue < int > colored_pixels;
+	vector < int > route;
+	while (bfs2(sinkNodes, sourceNodes, differMatrix,total_pixel, weightVector, route)) {
+		
+			int MINN = 200;
+			for (int i = 0; i < route.size() - 1; i++) {
+				for (int j = 0; j < weightVector[route[i]].size(); j++) {
+					if (weightVector[route[i]][j].first == route[i + 1]) {
+						MINN = min(MINN, weightVector[route[i]][j].second);
+					}
+				}
+
+			}
+
+			for (int i = 0; i < route.size() - 1; i++) {
+				for (int j = 0; j < weightVector[route[i]].size(); j++) {
+					if (weightVector[route[i]][j].first == route[i + 1]) {
+						weightVector[route[i]][j].second = weightVector[route[i]][j].second - MINN;
+					}
+					if (weightVector[route[i + 1]][j].first == route[i]) {
+						weightVector[route[i + 1]][j].second = weightVector[route[i + 1]][j].second + MINN;
+					}
+				}
+			}
+		
+	}
+	coloring(imageHeight, imageWidth, out_image, sinkNodes, sourceNodes, colored_pixels);
+	BFS(out_image, weightVector, imageWidth, differMatrix2, colored_pixels);
 }
 
-void max_flow(vector< list< pair<int, int> > > &adjlist, vector<int> &source,vector<int> &sink,
-	Mat out_image, int total_pixel, vector<pair<int, int>> &visitedPixel, queue<vector<int>> &queue,
-	vector<pair<int, int>> &anotherVisitedPixel, int height, int width) {
-	double minFlow = LONG_MAX;
-	vector < pair < int, int > > comb1;
-	comb1 = anotherVisitedPixel;
-	int visitedToken = 1;
-	vector<int> route;
-	//cout << "inside max flow" << endl;
-	bool visited;
-	visited = true;
-	while (visited == true) {
-		//cout << "inside first while loop" << endl;
-		visited = false;
-		while (!queue.empty()) {
-			//cout << "inside 2nd while loop" << endl;
-			again: vector<int> pathvector;
-			pathvector = queue.front();
-			queue.pop();
-			int lastelement = pathvector.back();
-			//cout << "last element" << lastelement << endl;
-			if (inspect(sink, lastelement) == 1) {
-				//cout << "inside if inspect" << endl;
-				route.clear();
-				route = pathvector;
-				visited = true;
-				//comb.clear();
-				anotherVisitedPixel.clear();
-				for (int i = 0; i < total_pixel; i++) {
-					pair<int, int> newpair = make_pair(i, 0);
-					//comb.push_back(newpair);
-					anotherVisitedPixel.push_back(newpair);
-				}
-				while (!queue.empty()) {
-					queue.pop();
-				}
-				for (int i = 0; i < source.size(); i++) {
-					vector<int> tempvector;
-					tempvector.push_back(source[i]);
-					queue.push(tempvector);
-				}
-				break;
-			}
-			list< pair<int, int> >::iterator itr1 = adjlist[lastelement].begin();
-			while (itr1 != adjlist[lastelement].end()) {
-				//cout << "inside itr1 " << endl;
-				//printf(" -> %d(%d)", (*itr1).first, (*itr1).second);
-				
-				if ((inspect1(pathvector, (*itr1).first)) && (((*itr1).second) > 0) && ((anotherVisitedPixel[(*itr1).first].second) == 0)){
-					//cout << "inside itr1 if" << endl;
-					vector < int > path(pathvector);
-					path.push_back((*itr1).first);
-					(anotherVisitedPixel[(*itr1).first].second) = 1;
-					queue.push(path);
-				}
-				++itr1;
-			}
-			//cout << "out itr1 " << endl;
-		}
-		if (visited == true) {
-			int minNum = 99999999;
-			for (int i = 0; i < route.size() - 1; i++) {
-				list< pair<int, int> >::iterator itr2 = adjlist[route[i]].begin();
-				while(itr2 != adjlist[route[i]].end()){
-					//cout << "inside itr2 flow" << endl;
-					if ((*itr2).first == route[i + 1]) {
-						if ((*itr2).second < minNum) {
-							minNum = (*itr2).second;
-						}
-					}
-					++itr2;
-				}
-			}
 
-			for (int i = 0; i < route.size() - 1; i++) {
-				list< pair<int, int> >::iterator itr3 = adjlist[route[i]].begin();
-				while (itr3 != adjlist[route[i]].end()) {
-					//cout << "inside itr3 flow" << endl;
-					if ((*itr3).first == route[i + 1]) {
-						(*itr3).second = (*itr3).second - minNum;
-
-					}
-					++itr3;
-				}
-			}
-			for (int i = 0; i < route.size() - 1; i++) {
-				list< pair<int, int> >::iterator itr4 = adjlist[route[i+1]].begin();
-				while (itr4 != adjlist[route[i+1]].end()) {
-					//cout << "inside itr4" << endl;
-					if ((*itr4).first == route[i]) {
-						(*itr4).second = (*itr4).second + minNum;
-					}
-					++itr4;
-				}
-
-			}
-		}
-	}
-	//cout << "inside coloring" << endl;
-	//coloring
-	for (int i = 0; i < height; i++){
-		for (int j = 0; j < width; j++){
-			Vec3b pixel;
-			pixel[0] = 255;
-			pixel[1] = 0;
-			pixel[2] = 0;
-			out_image.at<Vec3b>(i, j) = pixel;
-		}
-	}
-
-	Vec3b pixel2;
-	pixel2[0] = 0;
-	pixel2[1] = 0;
-	pixel2[2] = 255;
-
-	std::queue < int > qcolor;
-	int u = source[0];
-	qcolor.push(u);
-	int x1, x2;
-	x1 = u / width;
-	x2 = u % width;
-	out_image.at < Vec3b >(x1, x2) = pixel2;
-	int mino = 99999999;
-	BFS(qcolor, comb1, adjlist,out_image, width);
+//Covert the image to BW/grayscale image.
+void convertToGrayscale(Mat &in_image, Mat &gray_image, Mat &gradient_x, Mat &gradient_y) {
+	GaussianBlur(in_image, in_image, Size(3, 3), 0, 0, BORDER_DEFAULT);
+	cvtColor(in_image, gray_image, COLOR_BGR2GRAY);
+	Scharr(gray_image, gradient_x, CV_16S, 1, 0, 1, 0, BORDER_DEFAULT);
+	convertScaleAbs(gradient_x, gradient_x);
+	Scharr(gray_image, gradient_y, CV_16S, 0, 1, 1, 0, BORDER_DEFAULT);
+	convertScaleAbs(gradient_y, gradient_y);
 }
 
-/**
-@function main
-*/
-int main(int argc, char** argv)
-{
-	/* if (argc != 4) {
-		cout << "Usage: ../seg input_image initialization_file output_mask" << endl;
+void findMaxIntPixel(int imageHeight, int imageWidth, Mat gray_image) {
+	for (int i = 0; i < imageHeight; i++) {
+		for (int j = 0; j < imageWidth; j++) {
+			int currentPixel = gray_image.at < uchar >(i, j);
+			maxIntPixel = max(currentPixel, maxIntPixel);
+		}
+	}
+}
+
+// adds the edge between two pixels
+void setEdge(vector< pair<int, int> >  *adjlist, int k, int v, int weight) {
+	adjlist[k].push_back(make_pair(v, weight));
+}
+
+//main function
+int main(int argc, char * * argv) {
+	if (argc != 4) {
+		cout << "Usage:../seg input_image initialization_file output_mask" << endl;
 		return -1;
-	}*/
+	}
 
-	Mat in_image, gray_image;
-	Mat gradient;
-	Mat grad_x, grad_y;
+	Mat in_image;
+	in_image = imread(argv[1] /*, CV_LOAD_IMAGE_COLOR*/);
 
-	in_image = imread("simple.png"/*argv[1]*/);
-	if (!in_image.data)
-	{
+	if (!in_image.data) {
 		cout << "Could not load input image!!!" << endl;
 		return -1;
 	}
@@ -249,113 +196,111 @@ int main(int argc, char** argv)
 		cout << "Image does not have 3 channels!!! " << in_image.depth() << endl;
 		return -1;
 	}
-	convertToGrayscale(in_image, gray_image,grad_x,grad_y);
-	addWeighted(grad_x, 0.5, grad_y, 0.5, 0, gradient);
-	// the output image
+
 	Mat out_image = in_image.clone();
-	ifstream f("config.txt"/*argv[2]*/);
-	if (!f) {
+	ifstream configurationFile(argv[2]);
+
+	if (!configurationFile) {
 		cout << "Could not load initial mask file!!!" << endl;
 		return -1;
 	}
-	/*pair<int, int> sourcePair;
-	pair<int, int> sinkPair;
-	vector<pair<int,int>> source;
-	vector<pair<int,int>> sink;*/
-	vector<int> source;
-	vector<int> sink;
-	int width = in_image.cols;
-	int height = in_image.rows;
-	int total_pixel = width * height;
-	int n;
-	int pixelCount = 0;
-	int maxIntPixel = 0;
-	queue<vector<int>> queue;
-	f >> n;
 
-	// get the initial pixels
-	for (int i = 0; i < n; ++i) {
-		int row, col, w;
-		f >> row >> col >> w;
+	int imageWidth = in_image.cols;
+	int imageHeight = in_image.rows;
+	int total_pixel = imageHeight * imageWidth;
+	Mat gray_image, gradient_image;
+	Mat gradient_x, gradient_y;
 
-		if (row < 0 || row >= width || col < 0 || col >= height) {
-			cout << "I valid pixel mask!" << endl;
-			return -1;
+	//convert image to BW.
+	convertToGrayscale(in_image, gray_image, gradient_x, gradient_y);
+	addWeighted(gradient_x, 0.5, gradient_y, 0.5, 0, gradient_image);
+
+	//finding maximum intensity pixel
+	findMaxIntPixel(imageHeight, imageWidth, gray_image);
+
+	ispixelChecked = new bool[total_pixel];
+	vector < pair < int, int > > differMatrix;
+	vector < pair < int, int > >   *weightVector;
+	weightVector = new vector < pair < int, int > >[total_pixel];
+	/*weightVector.resize(total_pixel);*/
+	//calculate weights
+	for (int pixel = 0; pixel < total_pixel; pixel++) {
+		int x = pixel / imageWidth;
+		int y = pixel % imageWidth;
+		ispixelChecked[pixel] = false;
+		differMatrix.push_back(make_pair(pixel, 0));
+		differMatrix2.push_back(make_pair(pixel, 0));
+		if (pixel - 1 >= 0 && y - 1 >= 0) {
+			int leftPixelWeight = maxIntPixel - ((gradient_image.at<uchar>(x, y) + gradient_image.at<uchar>(x, y - 1)) / 2);
+			setEdge(weightVector, pixel, (x*imageWidth) + (y - 1), leftPixelWeight);
+		}
+		if (pixel + 1 < total_pixel && y + 1 < imageWidth) {
+			int rightPixelWeight = maxIntPixel - ((gradient_image.at<uchar>(x, y) + gradient_image.at<uchar>(x, y + 1)) / 2);
+			setEdge(weightVector, pixel, (x*imageWidth) + (y + 1), rightPixelWeight);
 		}
 
-		if (w == 1) { //foreground
-			/*sourcePair.first = row;
-			sourcePair.second = col;
-			source.push_back(sourcePair);*/
-			source.push_back((width*col) + row);
-			//source.push_back(make_pair(x,y));
-			//source.push_back((width * y) + x);
+		if (x - 1 >= 0 && pixel - imageWidth >= 0) {
+			int upPixelWeight = maxIntPixel - ((gradient_image.at<uchar>(x, y) + gradient_image.at<uchar>(x - 1, y)) / 2);
+			setEdge(weightVector, pixel, (((x - 1)*imageWidth) + y), upPixelWeight);
 		}
-		else { //background
-			/*sinkPair.first = row;
-			sinkPair.second = col;
-			sink.push_back(sinkPair);*/
-			sink.push_back((width*col) + row);
+
+		if (x + 1 < imageHeight && pixel + imageWidth < total_pixel) {
+			int downPixelWeight = maxIntPixel - ((gradient_image.at<uchar>(x, y) + gradient_image.at<uchar>(x + 1, y)) / 2);
+			setEdge(weightVector, pixel, (((x + 1)*imageWidth) + y), downPixelWeight);
+		}
+
+	}
+
+
+	//finding average of all weight edge 
+	int total_edges = 0;
+	int total_weight = 0;
+	for (int i = 0; i < total_pixel; i++) {
+		for (int j = 0; j < weightVector[i].size(); j++) {
+			total_weight = total_weight + weightVector[i][j].second;
+			total_edges++;
 		}
 	}
 
-	//getting the maximum intensity pixel - maxIntPixel
-	for (int i = 0; i < width; i++) {
-		for (int j = 0; j < height; j++) {
-			pixelCount++;
-			int currentPixel = gray_image.at < uchar >(j, i);
-			if (currentPixel > maxIntPixel) {
-				maxIntPixel = currentPixel;
+	int average_weight = total_weight / total_edges;
+	for (int i = 0; i < total_pixel; i++) {
+		for (int j = 0; j < weightVector[i].size(); j++) {
+			if (weightVector[i][j].second < average_weight) {
+				weightVector[i][j].second = 1;
+			}
+			else {
+				weightVector[i][j].second = 100;
 			}
 		}
 	}
-	/*cout << "total pixel" << pixelCount << endl;
-	cout << "maxIntPixel" << maxIntPixel << endl;
-	cout << "row " << "column " << "weights" << endl;*/
-	vector< list< pair<int, int> > > adjlist(total_pixel);
-	vector<pair<int, int>> visitedPixel, anotherVisitedPixel;
-	//weight calculation
-	for (int pixel = 0; pixel < total_pixel; ++pixel) {
-		int x = pixel / width;
-		int y = pixel % width;
-		
-		visitedPixel.push_back(make_pair(pixel, 0));
-		anotherVisitedPixel.push_back(make_pair(pixel, 0));
-		if (pixel - 1 >= 0 && y - 1 >= 0) {
-			int leftPixelWeight = maxIntPixel - ((gray_image.at<uchar>(x, y) + gray_image.at<uchar>(x, y - 1))/2);
-			setEdge(adjlist, pixel, pixel - 1, leftPixelWeight);
+	int n;
+	configurationFile >> n;
+	vector<int> sourceNodes;
+	vector<int> sinkNodes;
+	for (int i = 0; i < n; ++i) {
+		int x_axis, y_axis, w;
+		configurationFile >> x_axis >> y_axis >> w;
+		if (x_axis < 0 || x_axis >= imageWidth || y_axis < 0 || y_axis >= imageHeight) {
+			cout << "Invalid pixel mask!" << endl;
+			return -1;
 		}
-		if (pixel + 1 < total_pixel && y + 1 < width) {
-			int rightPixelWeight = maxIntPixel - ((gray_image.at<uchar>(x, y) + gray_image.at<uchar>(x, y + 1)) / 2);
-			setEdge(adjlist, pixel, pixel + 1, rightPixelWeight);
+		int position = (imageWidth * y_axis) + x_axis;
+		if (w == 1) {//sourceNodes nodes
+			sourceNodes.push_back(position);
 		}
-
-		if (x + 1 < height && pixel + width < total_pixel) {
-			int downPixelWeight = maxIntPixel - ((gray_image.at<uchar>(x, y) + gray_image.at<uchar>(x+1, y)) / 2);
-			setEdge(adjlist, pixel, pixel + width, downPixelWeight);
+		else {//sinkNodes nodes
+			sinkNodes.push_back(position);
 		}
-
-		if (x - 1 >= 0 && pixel - width >= 0) {
-			int upPixelWeight = maxIntPixel - ((gray_image.at<uchar>(x, y) + gray_image.at<uchar>(x-1, y)) / 2);
-			setEdge(adjlist, pixel, pixel - width, upPixelWeight);
-		}
-
 	}
-	//printList(adjlist,visitedPixel);
-	for (int i = 0; i < source.size(); i++) {
-		vector<int> tempvector;
-		tempvector.push_back(source[i]);
-		queue.push(tempvector);
-	}
-	
-	max_flow(adjlist, source, sink, out_image, total_pixel, visitedPixel, queue, anotherVisitedPixel, height,width);
-	// write it on disk
-	imwrite("output.png"/*argv[3]*/, out_image);
+
+	// min cut max flow 
+	maxFlow_minCut(sinkNodes, sourceNodes, differMatrix, total_pixel, weightVector, imageHeight, imageWidth, out_image);
+	imwrite(argv[3], out_image);
+	// also display them both
 	namedWindow("Original image", WINDOW_AUTOSIZE);
 	namedWindow("Show Marked Pixels", WINDOW_AUTOSIZE);
 	imshow("Original image", in_image);
 	imshow("Show Marked Pixels", out_image);
-	imshow("gradient image", gradient);
 	waitKey(0);
 	return 0;
 }
